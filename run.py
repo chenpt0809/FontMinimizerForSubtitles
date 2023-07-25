8cc9d7dfc9be1153f46a8d15ece0338f19213f05
chenpt <50280889+chenpt0809@users.noreply.github.com>
2023-07-25 18:09:39 +0800

    ADD: Get all the font files in the directory.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import json
+from pathlib import Path
 
 
 class font:
@@ -12,4 +13,15 @@ class font:
         self.cache.append(json.load(file_path))
         return True
 
-
+    def get_cache(self, dir_path):
+        if not os.path.exists(dir_path):
+            return False
+        file_list = list()
+        path = Path(dir_path)
+        c = 0
+        for file in path.rglob('*.[TtOo][Tt][FfCc]'):
+            file_list.append(file)
+            c += 1
+            print(f"\r获取字体文件数量：{c}", end='', flush=True)
+        else:
+            print(f"\r成功获取字体文件数量：{c}", flush=True)
